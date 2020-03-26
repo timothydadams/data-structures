e2bbcd5fda11fe36ba8c92eb84f8663fe8536f02
Tim Adams <adams.timothy.d@gmail.com>
2020-03-25 19:03:25 -0700

    Working on functional/stack.js

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,15 +5,23 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
-
   someInstance.enqueue = function(value) {
+  //should add a string to the back of the queue
   };
 
   someInstance.dequeue = function() {
+  //Remove and return the string at the front of the queue
   };
 
   someInstance.size = function() {
+  //Return the number of items in the queue
+  count = 0
+  for (var key in obj) {
+  count ++;
+  }
+  return count - 1;
   };
+
 
   return someInstance;
 };
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,29 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    //search keys for highest value
+    // if (someInstance === {}) {
+    //   var index = 0;
+    // }
+    var index = -1;
+    for (var key in someInstance) {
+      index = (someInstance[key] > index) ? someInstance[key] : index;
+    }
+    someInstance[index] = value;
+    return someInstance;
   };
 
   someInstance.pop = function() {
   };
 
   someInstance.size = function() {
+    var count = 0;
+    for (var key in someInstance) {
+      if (Number(key)) {
+        count ++;
+      }
+    }
+    return count;
   };
 
   return someInstance;
