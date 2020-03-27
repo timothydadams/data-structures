66e88a1bd27706e8a3c37851c609bdfd5321ea85
Tim Adams <adams.timothy.d@gmail.com>
2020-03-26 20:31:08 -0700

    Finished pseudoclassical pattern and ES6 pattern for both stack and queue data structures

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,29 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
 
+Queue.prototype.enqueue = function(value) {
+  var max = -1;
+  for (var key in this.storage) {
+    max = (Number(key) > max) ? Number(key) : max;
+  }
+  this.storage[max + 1] = value;
+}
 
+Queue.prototype.dequeue = function () {
+  var min = Infinity; // ... and beyond
+  for (var key in this.storage) {
+    min = (Number(key) < min) ? Number(key) : min;
+  }
+  var value = this.storage[min];
+  delete this.storage[min];
+  return value;
+}
+
+Queue.prototype.size = function () {
+  var count = 0;
+  for (var key in this.storage) {
+    count ++;
+  }
+  return count;
+}
