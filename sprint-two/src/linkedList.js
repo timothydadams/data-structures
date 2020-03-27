ceabc088904f3c222c2daea518697ae99b236f0d
Tim Adams <adams.timothy.d@gmail.com>
2020-03-27 16:46:52 -0700

    Working on linked-list.contains function

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,34 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    //console.log(newNode);
+    if (list.head === null) {
+      list.head = newNode;
+    }
+    list.head.next = newNode;
+    list.tail = newNode;
+    console.log(list);
   };
 
   list.removeHead = function() {
+    //save the value at head
+    var value = list.head.value;
+    list.head = list.head.next;
+    return value;
   };
 
   list.contains = function(target) {
+
+  //iterate over the object
+  var listItem = list;
+  while (listItem !== null) {
+    if (list.value === target) {
+      return true;
+      l
+    }
+  }
+  return false;
   };
 
   return list;
@@ -26,4 +48,9 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
- */
+
+addToTail: O(1)
+removeHead: O(1)
+contains: O(n)
+
+*/
