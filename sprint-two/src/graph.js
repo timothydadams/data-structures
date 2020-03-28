68c5aee38a71e8472f0514569aad9117ea3ba75e
Peter Knipp <peteraknipp@gmail.com>
2020-03-28 11:05:01 -0400

    Fix trees.js and code graph.js (the latter of which is not passing all tests)

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,39 +1,58 @@
 
 
-// Instantiate a new graph
+// Instantiate a new graph ...
+// ... in pseudoclassical style.
 var Graph = function() {
+  this.storage = {};
+  this.edges = {};
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.storage[node] = node;
+  this.edges[node] = {};
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return (Boolean(this.storage[node]));
 };
 
 // Removes a node from the graph.
+// We may also need to remove zero or more edges, if this does not occur automatically.
 Graph.prototype.removeNode = function(node) {
+  for(let otherNode in this.storage){
+    delete this.edges[node][otherNode];
+    delete this.edges[otherNode][node];
+  }
+  delete this.storage[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  return this.edges[fromNode][toNode];
 };
 
 // Connects two nodes in a graph by adding an edge between them.
+// This'll be a symmetric function of its inputs, because graph is unidirectional.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.edges[fromNode][toNode] = true;
+  this.edges[toNode][fromNode] = true;
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  delete this.edges[fromNode][toNode];
+  delete this.edges[toNode][fromNode];
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for(let node in this.storage){
+    cb(node);
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,23 +1,38 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // PAK fixed by changing "null" to "[]"
+  extend(newTree, treeMethods);
   return newTree;
+};
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  if(this.value === target){
+    return true;
+  }else{
+    for (let child of this.children){
+      if(child.contains(target)){
+        return true;
+      }
+    }
+    return false;
+  }
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
+addChild: O(1)
+contains: O(n)
  */
