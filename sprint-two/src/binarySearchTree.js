7741af200dd7466db255d251a783d58f4e70d573
Peter Knipp <peteraknipp@gmail.com>
2020-03-29 11:50:56 -0400

    Insert "new" in line 5 of BSTspec

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,48 @@
 var BinarySearchTree = function(value) {
-};
+	this.value = value;
+	this.left = null;
+	this.right = null;
+}
 
+BinarySearchTree.prototype.insert = function(value) {
+  if (value < this.value) {
+    if (this.left === null) {
+      this.left = new BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  } else {
+    if (this.right === null) {
+      this.right = new BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  }
+}
 
+BinarySearchTree.prototype.contains = function(value) {
+	console.log(this);
+  if (value === this.value){
+    return true;
+  }
+  if (value < this.value) {
+    if (this.left !== null) {
+      this.left.contains(value);
+    } else {
+      return false;
+    }
+	}
+	if (value > this.value) {
+    if (this.right !== null) {
+      this.right.contains(value);
+    } else {
+			return false;
+    }
+  }
+}
+
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+}
 /*
  * Complexity: What is the time complexity of the above functions?
  */
